7aa1925d82c44a3e2d2bf64009537a788e078650
Mardragox <krzysieksot@gmail.com>
2024-09-13 11:32:50 +0200

    sitoeratotenesa essa essa

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,41 @@ public class Main {
 
         Gra gra1 = new Gra();
         gra1.zagraj();
+        System.out.println("pierwsze: " + sitoEratostenesa(1000));
 
     }
+
+    //do kartkówki - sito erastotenesa
+
+    static ArrayList<Integer> sitoEratostenesa(int liczba){
+        //przygotowanie listy z wartościami logicznymi
+
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 0; i < liczba; i++) {
+            logiczne.add(true);
+        }
+        //wykreślanie wielokrotności
+        for (int i = 0; i < liczba; i++) {
+            if (logiczne.get(i).equals(true)){
+                //w ykreślanie(zmiana na false)
+
+                for (int k = i*2; k<liczba ; k = k + i){
+                    logiczne.set(k,false);
+                }
+            }
+        }
+
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i < logiczne.size(); i++) {
+            if (logiczne.get(i).equals(true)){
+                pierwsze.add(i);
+            }
+
+        }
+        return pierwsze;
+    }
+
 
 }
