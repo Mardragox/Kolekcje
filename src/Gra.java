f4dfaa59fd1966a9be770a55ec35109d33a69c00
Mardragox <krzysieksot@gmail.com>
2024-09-13 10:48:31 +0200

    funkcja gra

---
+++ src/Gra.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Gra {
+
+    private ArrayList<Integer> wylosowane;
+    private LinkedList<Integer> wpisane;
+    private ArrayList<Integer> trafione;
+
+
+
+
+    private void wylosujLiczby(int ile){
+
+        wylosowane = new ArrayList<>();
+        int liczba;
+        for (int i = 0; i < ile; i++) {
+            liczba = (int)(Math.random()*10);
+
+            while(wylosowane.contains(liczba)){
+                liczba = (int)(Math.random()*10+1);
+            }
+
+
+            wylosowane.add(liczba);
+        }
+
+
+    }
+    private void wczytajLiczby(int ile){
+
+        wpisane = new LinkedList<>();
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj " + ile +" liczb");
+        for (int i = 0; i < ile; i++) {
+            wpisane.add(klawiatura.nextInt());
+        }
+
+    }
+
+
+    private void wypiszListe(List<Integer> lista){
+        for (Integer element:lista){
+            System.out.print(element+", ");
+        }
+    }
+
+
+    private void sprawdzKtorePowtarzajaSie(){
+        trafione = new ArrayList<>();
+        for (Integer element:wylosowane) {
+            if(wpisane.contains(element)){
+                trafione.add(element);
+            }
+        }
+
+    }
+
+    public void zagraj(){
+
+        wylosujLiczby(20);
+        wczytajLiczby(5);
+        sprawdzKtorePowtarzajaSie();
+        System.out.println("liczby wylosowane");
+        wypiszListe(wylosowane);
+        System.out.println("liczby wpisane");
+        wypiszListe(wpisane);
+        System.out.println("Liczby trafione");
+        wypiszListe(trafione);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,79 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> wylosowane = wylosujLiczby(7);
-        LinkedList<Integer> wpisane = wczytajLiczby(4);
-        ArrayList<Integer> trafione = sprawdzKtorePowtarzajaSie(wylosowane, wpisane);
-        System.out.println("liczby wylosowane");
-        wypiszListe(wylosowane);
-        System.out.println("liczby wpisane");
-        wypiszListe(wpisane);
-        System.out.println("Liczby trafione");
-        wypiszListe(trafione);
+
+        Gra gra1 = new Gra();
+        gra1.zagraj();
 
     }
 
-    /**
-     * wylosujLiczby - metoda która losuje liczby całkowite z zakresu 1 - 10
-     * @param ile - liczba wylosowanych wartości
-     * @return - lista z wylosowanymi wartościami
-     */
-    private static ArrayList<Integer> wylosujLiczby(int ile){
-        System.out.println("Wylosowane liczby z funkcji prywatnej: ");
-        ArrayList<Integer> listaLosowychBezPowtorzen = new ArrayList<>();
-        int liczba;
-        for (int i = 0; i < ile; i++) {
-            liczba = (int)(Math.random()*10);
-
-            while(listaLosowychBezPowtorzen.contains(liczba)){
-                liczba = (int)(Math.random()*10+1);
-            }
-
-
-            listaLosowychBezPowtorzen.add(liczba);
-        }
-
-        return listaLosowychBezPowtorzen;
-    }
-
-    /**
-     * wczytajLiczby - liczby wczytywane z klawiatury komputera
-     * @param ile - ile lkiczb wpisać
-     * @return - zwraca wpisane liczby
-     */
-    private static LinkedList<Integer> wczytajLiczby(int ile){
-
-        LinkedList<Integer> wpisane = new LinkedList<>();
-        Scanner klawiatura = new Scanner(System.in);
-        System.out.println("Podaj " + ile +" liczb");
-        for (int i = 0; i < ile; i++) {
-            wpisane.add(klawiatura.nextInt());
-        }
-        return wpisane;
-    }
-
-    /**
-     * @param lista - lsita
-     */
-    private static void wypiszListe(List<Integer>lista){
-        for (Integer element:lista){
-            System.out.print(element+", ");
-        }
-    }
-
-
-    /**
-     * @param wpisane - liczby wpisywane z klawy
-     * @param wylosowane - liczby RNG
-     * @return - zwreaca te które są trafione
-     */
-    private static ArrayList<Integer> sprawdzKtorePowtarzajaSie(ArrayList<Integer>wpisane, LinkedList<Integer>wylosowane){
-        ArrayList<Integer> trafione = new ArrayList<>();
-        for (Integer element:wylosowane) {
-            if(wpisane.contains(element)){
-                trafione.add(element);
-            }
-        }
-        return trafione;
-    }
 }
